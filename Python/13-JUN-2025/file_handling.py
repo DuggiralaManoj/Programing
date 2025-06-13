08d82545294162e5c3b4a9c8855cbf670cee24c6
DuggiralaManoj <dm9625178@gmil.com>
2025-06-13 15:12:03 -0500

    Adding python files and screenshots of its output linux commands and screenshot of its output and git push commnds

---
+++ Python/13-JUN-2025/file_handling.py
@@ -0,0 +1,26 @@
+#f=open("file_1","w")
+#f.write("hello")
+
+# f1=open("file1","read")
+# f1=f1.read()
+# print(f1)
+
+# f2=open("file2","a")
+# f2.write("Man")
+
+# f3=open("file_1","r+")
+# print(f3.read())
+# f3.write("What")
+
+# f4=open("file2","w+")
+# f4.write("Supp")
+# f4.seek(0)
+# print(f4.read())
+
+# f5=open("file2","a+")
+# f5.write("nothing")
+# f5.seek(0)
+# print(f5.read())
+
+
+
