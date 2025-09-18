e169a6e0fb625a24a00a20de2f1d4b52cdce42ae
Duggirala Manoj <dm9625178@gmail.com>
2025-09-18 18:07:46 +0530

    Adding Specal variable __name__ functionalty and its use with examples

---
+++ Python/17-SEP-2025/name.py
@@ -0,0 +1,16 @@
+print("from name")
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def mul(a,b):
+    print(a*b)
+if __name__=="__main__" :
+    print("from name")
+    x=int(input("enter x value:"))
+    y=int(input("enter y value:"))
+    add(x,y)
+    sub(x,y)
+    mul(x,y)
+
+
